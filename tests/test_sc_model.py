330d94c9fc6dcefecab0b954241268ecde3cee67
Jebbex <107323805+Jebbex1@users.noreply.github.com>
2025-10-09 17:02:57 +0300

    Added tests for SC model and deleted old ones

---
+++ tests/test_sc_model.py
@@ -0,0 +1,28 @@
+from functools import partial
+from sconn.server.server import Server
+from sconn.client.client import Client
+from sconn.protocol.constants import ConnectionModels
+from ssl import SSLSocket
+from sconn.utils.config_interface import ServerConfig
+
+
+def reply_with_sent_once(skt: SSLSocket, /) -> None:
+    skt.send(skt.recv())
+
+
+def test_sc_model() -> None:
+    server = Server(config_path =           "testing_server_config.yaml", 
+                    handler_exit_function = partial(reply_with_sent_once))
+    server.start()
+    
+    
+    client = Client(ConnectionModels.SERVER_CLIENT,
+                    config_path="testing_client_config.yaml")
+    client.connect()
+    
+    data = b"WAAAAAAAAAAAAA"
+    client.send(data)
+    assert client.recv() == data
+    
+    client.disconnect()
+    server.stop()
