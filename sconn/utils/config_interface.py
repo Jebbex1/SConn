4013fb2b1674e89f595924f2644270084c1fbd86
Jebbex <107323805+Jebbex1@users.noreply.github.com>
2025-10-11 20:47:49 +0300

    Added automatic config file validation to ensure all the settings are present, fixed typos

--- sconn/utils/config_interface.py
+++ sconn/utils/config_interface.py
@@ -8,18 +8,28 @@ import sys, os
 
 sys.path.insert(0, "../..")  # add the relative path of the default settings to the path var
 
-SERVER_DEFAULT_CONFIG = open("defualt_server_config.yaml", "r").read()
-CLIENT_DEFAULT_CONFIG = open("defualt_client_config.yaml", "r").read()
+SERVER_DEFAULT_CONFIG_PATH = "default_server_config.yaml"
+CLIENT_DEFAULT_CONFIG_PATH = "default_client_config.yaml"
+
+
+def get_dict_keys(d: dict) -> list:
+    key_list = []
+    for k, v in d.items():
+        key_list.append(k)
+        if isinstance(v, dict):
+            key_list.append(get_dict_keys(v))
+            
+    return key_list
 
 
 class BaseConfig:
     def __init__(self, config_file_path: str, server_side: bool):
         if not os.path.exists(config_file_path):
             f = open(config_file_path, "w")
-            defualt_config = SERVER_DEFAULT_CONFIG if server_side else CLIENT_DEFAULT_CONFIG
-            f.write(defualt_config)
+            default_config = open(SERVER_DEFAULT_CONFIG_PATH, "r").read() if server_side else open(CLIENT_DEFAULT_CONFIG_PATH, "r").read()
+            f.write(default_config)
             f.close()
-        self.config_yaml = load(open(config_file_path, "r"), Loader=Loader)
+        self.config_yaml = safe_load(open(config_file_path, "r"))
         self.config_file_path = config_file_path
     
     def use_mtls(self) -> bool:
@@ -35,6 +45,7 @@ class BaseConfig:
 class ServerConfig(BaseConfig):
     def __init__(self, config_file_path: str):
         super().__init__(config_file_path, True)
+        ServerConfig.validate_server_config_file(self.config_yaml)
         
     def get_tls_certificate_paths(self) -> tuple[str, str]:
         return self.config_yaml["certificate_path"], self.config_yaml["certificate_key_path"]
@@ -50,10 +61,17 @@ class ServerConfig(BaseConfig):
     
     def supports_ubg_model(self) -> bool:
         return self.config_yaml["supported_models"]["ubg_model"]
+    
+    @staticmethod
+    def validate_server_config_file(config_dict: dict) -> None:
+        server_example_config = safe_load(open(SERVER_DEFAULT_CONFIG_PATH, "r").read())
+        assert get_dict_keys(server_example_config) == get_dict_keys(config_dict), \
+            "Opened config file that doesn't have all the necessary settings!"
 
 class ClientConfig(BaseConfig):
     def __init__(self, config_file_path: str):
         super().__init__(config_file_path, False)
+        ClientConfig.validate_client_config_file(self.config_yaml)
         
     def get_server_hostname(self) -> str:
         return self.config_yaml["server_hostname"]
@@ -63,3 +81,9 @@ class ClientConfig(BaseConfig):
     
     def get_mtls_certificate_path(self) -> str:
         return self.config_yaml["mtls_client_certificate"]
+    
+    @staticmethod
+    def validate_client_config_file(config_dict: dict) -> None:
+        client_example_config = safe_load(open(CLIENT_DEFAULT_CONFIG_PATH, "r").read())
+        assert get_dict_keys(client_example_config) == get_dict_keys(config_dict), \
+            "Opened config file that doesn't have all the necessary settings!"
